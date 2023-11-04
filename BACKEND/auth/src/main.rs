bdb82600b0f390115e717415209c247282d2e574
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-11-04 02:23:58 +0100

    chore: final cleanup

--- BACKEND/auth/src/main.rs
+++ BACKEND/auth/src/main.rs
@@ -1,10 +1,17 @@
-use sdk;
 use anyhow::Result;
+use dotenvy::dotenv;
+
+use sdk;
 
 use auth::application::app_data::AppData;
 
 #[tokio::main]
 async fn main() -> Result<()>{
+    dotenv().ok();
+
+    let PORT = env!("PORT");
+    let ENVIRONMENT = env!("ENVIRONMENT");
+
     let a = AppData::new("a".to_owned(), "b".to_owned(), "s".to_owned());
 
     let a = sdk::add(1, 3);
